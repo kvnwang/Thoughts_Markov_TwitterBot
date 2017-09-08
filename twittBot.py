62386e363633dfc16d209965b4effac1c2dac1d7
Kevin <kwang30@gmail.com>
2017-09-08 00:24:51 -0400

    updating reddit

--- twittBot.py
+++ twittBot.py
@@ -1,15 +1,15 @@
 import praw
-import json
-import requests
 import tweepy 
 import time
+import markovify
+
 
 
 number_tweets=5
 SUBREDDITNAME='r\ShowerThoughts'
 CLIENT_ID='XXX'
 CLIENT_SECRET='XXX'
-PASSWORD='XXX'
+PASSWORD='XXXX'
 USER_AGENT='XXX'
 USERNAME='XXX'
 
@@ -19,46 +19,36 @@ ACCESS_TOKEN='XXX'
 ACCESS_TOKEN_SECRET='XXX'
 
 def reddit_conenction(subreddit):
-	r = praw.Reddit(
-		client_id=CLIENT_ID, 
-		client_secret=CLIENT_SECRET,  
-		password=PASSWORD, 
-		user_agent=USER_AGENT, 
-		username=USERNAME)
-	subR=r.get.subreddit(subreddit)
-	return subR
+	reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,  password=PASSWORD, user_agent=USER_AGENT, username=USERNAME)
+	sub = reddit.subreddit(subreddit)
+	return sub
+
 
 def get_Reddit_posts(subreddit):
 	post_titles = []
-	post_links = []
 	post_ids = []
-	for submission in subreddit.get_hot(limit=5):
-		if already_tweeted(submission.id): 
-			p_id=submission.id
-			post_link=submission.url
-			post_title=clean_title(submission.title)
-			print (post_title)
+	for submission in subreddit.hot(limit=20):
+		#if already_tweeted(submission.id):
+			new_title=cleanup_text(submission.title)
+			post_titles.append(new_title)
+			post_ids.append(submission.id)
+	return post_titles, post_ids
 
-			post_titles.append(post_title)
-			post_links.append(post_link)
-			post_ids.append(post_id)
-	return post_titles, post_links, post_ids
-
-def clean_title(title):
-	characters=len(title)
-	if len(title)<=140: 
-		return title
-	elif len(title>=140):
-		return title[:140]
+def cleanup_text(tweet):
+	characters=len(tweet)
+	if len(tweet)<=140: 
+		return tweet
+	elif len(tweet)>=140:
+		return tweet[:140]
 	else:
 		return ""
 
 def already_tweeted(p_id):
-    with open(posted_tweeets, 'r') as file:
-        for line in file:
-            if pid in line:
-            	return True
-    return False
+	with open('posted_tweets.txt', 'r') as file:
+		for line in file.read().splitlines():
+			if post_id == line:
+				return True
+	return False
 
 def twitter_connection():
 	print ("Authenticizing Twitter Connection")
@@ -70,27 +60,36 @@ def twitter_connection():
 	print('Authenticated.')
 	return twitter
 
-def log_tweet(post_id):
-    with open(posted_tweeets, 'a') as log:
-        log.write(str(post_id) + '\n')
+# def log_tweet(post_id):
+# 	count=0
+#     with open(posted_tweeets, 'a') as log:
+#         log.write(str(post_id[count]) + '\n')
+#         count=count+1
 
-# def issue_tweets(api):
-# 	for post, post_id in zip(post_titles, post_ids):
-#         post_text = strip_title(post)
-#         print('[bot] Posting this link on Twitter')
-#         print(post_text)
-#         api.update_status(post_text)
-#         log_tweet(post_id)
-#         time.sleep(DELAY_BETWEEN_TWEETS)
+def create_msg(tweets):
+	new_tweets=[]
+	for tweet in tweets:
+		model = markovify.Text(tweets, state_size=1)
+		new_tweet=model.make_short_sentence(140)
+		new_tweets.append(new_tweet)
+	return new_tweets
+
+
+def post_tweet(api, tweets):
+	for tweet in tweets:
+		api.update_status(post_text)
+		time.sleep(5*60)
 
 def main():
 	print("This is a Reddit Twitter Bot")
-	subreddit = reddit_conenction(SUBREDDIT_TO_MONITOR)
-	post_titles, post_links, post_ids=get_Reddit_posts(subreddit)
-	# chains=generate_chains()
+	subreddit = reddit_conenction('Showerthoughts')
+	post_titles, post_ids=get_Reddit_posts(subreddit)
+	#logTweets(post_ids)
+	#chains=generate_chains()
+	tweets=create_msg(post_titles)
 	# messages=generate_messages(post_titles, post_links, post_ids)
-	# twit=twitter_connection()
-	#issue_tweets(twit)
+	twit=twitter_connection()
+	post_tweet(twit)
 
 
 
